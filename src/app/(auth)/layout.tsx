675011835dde1c01b3324d290ac4bb2c6a065773
Denis Lutsevich <debibidon@gmail.com>
2024-02-20 14:47:33 +0300

    fix auth layout styles

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -5,12 +5,13 @@ import classes from './layout.module.css';
 export default function AuthLayout({children}: Readonly<{ children: React.ReactNode; }>) {
     return (
         <div className={classes["auth-layout-container"]}>
-            <Card className={classes.card}>
-                <CardContent className={classes["card-content"]}>
-                    {children}
-                </CardContent>
-            </Card>
-            <div className={classes["card-background"]}></div>
+            <div className={classes["card-container"]}>
+                <Card className={classes.card}>
+                    <CardContent className={classes["card-content"]}>
+                        {children}
+                    </CardContent>
+                </Card>
+            </div>
         </div>
     );
 }
